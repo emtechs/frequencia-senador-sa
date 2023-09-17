5b85a5c01a7788ae8210b04ac2e484daf1ac3b4c
Lucas <lucassoares2007@hotmail.com>
2023-09-17 03:54:50 -0300

    feat: images

--- src/shared/services/api.ts
+++ src/shared/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const token = localStorage.getItem('@EMTechs:token')
-const serverUrl = 'https://api-frequencia-senador-sa.emsolucoestecnologicas.com.br/'
+const serverUrl = 'https://system-api.vercel.app/'
 const localServer = 'http://localhost:4002/'
 
 const localApi = axios.create({
